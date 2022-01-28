f521c41d9bb56e5aa587d395c794dbb764b1582f
skgx <saurabhgupta2131@gmail.com>
2022-01-28 23:11:14 +0530

    added html file in home page

--- hello/home/views.py
+++ hello/home/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("this is homepage")
+    return render(request,'index.html')
+    #return httpResponse("this is home page")
 
 def about(request):
     return HttpResponse("this is about page")
