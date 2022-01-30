8fefed5b2fb50fa1efdc005bf7241b86f254ce4b
skgx <saurabhgupta2131@gmail.com>
2022-01-30 13:43:06 +0530

    init

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [
     # BASE_DIR / "static",
     # '/var/www/static/',
 ]
+
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
