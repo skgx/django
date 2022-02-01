d155f851e5b81010024eaaffbbb222db7ac271f6
skgx <saurabhgupta2131@gmail.com>
2022-02-01 11:47:30 +0530

    init

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.contrib.messages import constants as messages
 # Create your views here.
 
 def index(request):
@@ -7,6 +10,7 @@ def index(request):
         'variable1':"Saurabh here!",
         'variable2':'How are you'
     }
+    messages.success(request,"this is test")
     return render(request,'index.html',context)
     #return httpResponse("this is home page")
 
@@ -19,5 +23,13 @@ def services(request):
     #return HttpResponse("this is services page")
 
 def contact(request):
-    return render(request,'contact.html',)
+ if request.method=='POST': 
+     name=request.POST.get('name')
+     email=request.POST.get('email')
+     desc=request.POST.get('desc')
+     contact=Contact(name=name,email=email,desc=desc,date=datetime.today())
+     contact.save()
+     messages.success(request, 'message sent!')
+ return render(request,'contact.html',)
     #return HttpResponse("this is contacts page")
+
