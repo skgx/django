7dd408a8b21686a9649eb371ca06d922295a8521
skgx <saurabhgupta2131@gmail.com>
2022-02-03 22:34:13 +0530

    added staic files

--- saurabh/travello/views.py
+++ saurabh/travello/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     #return HttpResponse("hello world")
     return render(request,'index.html')
+
+def add(request):
+    return HttpResponse()
